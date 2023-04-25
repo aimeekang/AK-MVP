08637f08f7ba20440e88433d1915854b0220055e
aimeekang <asrkang@gmail.com>
2023-04-25 11:38:04 -0700

    add more information to climbs query

--- server/models/climbs.js
+++ server/models/climbs.js
@@ -3,28 +3,35 @@ const db = require('../database/db.js');
 const getClimbs = (req) => {
   const searchTerm = req.body.term;
   const query = `
-    WITH c AS (
-      SELECT *
-      FROM climbs
-      WHERE LOWER(name) LIKE '%${searchTerm}%'
-    ),
-    a AS (
+    WITH a AS (
       SELECT *
       FROM areas
       WHERE LOWER(name) LIKE '%${searchTerm}%'
+    ),
+    c AS (
+      SELECT *
+      FROM climbs
+      WHERE LOWER(name) LIKE '%${searchTerm}%'
     )
     SELECT json_build_object(
-      'routes', (
-        SELECT json_agg(json_build_object(
-          'name', c.name
-        ))
-        FROM c
-      ),
       'areas', (
         SELECT json_agg(json_build_object(
-          'name', a.name
+          'id', a.id,
+          'name', a.name,
+          'path_tokens', a.path_tokens,
+          'total_climbs', a.total_climbs
         ))
         FROM a
+      ),
+      'routes', (
+        SELECT json_agg(json_build_object(
+          'id', c.id,
+          'name', c.name,
+          'yds', c.yds,
+          'fa', c.fa,
+          'type', c.type
+        ))
+        FROM c
       )
     )
   `;
