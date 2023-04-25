822e4a36e3912fedfd9823284beb768f8090f9d6
aimeekang <asrkang@gmail.com>
2023-04-25 11:15:20 -0700

    add areas data into models

--- server/models/climbs.js
+++ server/models/climbs.js
@@ -7,14 +7,24 @@ const getClimbs = (req) => {
       SELECT *
       FROM climbs
       WHERE LOWER(name) LIKE '%${searchTerm}%'
+    ),
+    a AS (
+      SELECT *
+      FROM areas
+      WHERE LOWER(name) LIKE '%${searchTerm}%'
     )
-
     SELECT json_build_object(
       'routes', (
         SELECT json_agg(json_build_object(
           'name', c.name
         ))
         FROM c
+      ),
+      'areas', (
+        SELECT json_agg(json_build_object(
+          'name', a.name
+        ))
+        FROM a
       )
     )
   `;
